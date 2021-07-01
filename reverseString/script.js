3c17d83c1cd0920fda40cb4645d06f6e079c8a6f
JoyceKell <joycekellywork@gmail.com>
2021-06-30 23:01:44 -0300

    questão reverseString

---
+++ reverseString/script.js
@@ -0,0 +1,9 @@
+let newStr = "";
+
+function reverseAstring(str) {
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+    console.log(newStr);
+  }
+}
+let resultado = reverseAstring("hello");

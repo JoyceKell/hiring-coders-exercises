d9ceb0b6f358f3778788e7f4b11615f42c0c901f
JoyceKell <joycekellywork@gmail.com>
2021-06-30 23:08:23 -0300

    questão convertToFahrenheit

---
+++ ConvertToFahrenheit/script.js
@@ -0,0 +1,5 @@
+function convertToFahrenheit(value) {
+  return value * 1.8 + 32;
+}
+let result = convertToFahrenheit(40);
+console.log(`o valor em fahrenheit é ${result}`);
--- reverseString/script.js
+++ reverseString/script.js
@@ -3,7 +3,7 @@ let newStr = "";
 function reverseAstring(str) {
   for (let i = str.length - 1; i >= 0; i--) {
     newStr += str[i];
-    console.log(newStr);
   }
+  console.log(newStr);
 }
 let resultado = reverseAstring("hello");
